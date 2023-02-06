7d08bca5c9f13ff97ca5cf58eaebbf517c2defcc
Vinicius Santos <viniciusantos.florencio@gmail.com>
2023-02-06 16:41:55 -0300

    Initial Commit

---
+++ memorize-study/memorize_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorize_studyApp.swift
+//  memorize-study
+//
+//  Created by Vinicius  Santos on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct memorize_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
