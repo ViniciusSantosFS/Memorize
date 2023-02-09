06e0b54f892c99b97f9c9ae2913f08258995979b
Vinicius Santos <viniciusantos.florencio@gmail.com>
2023-02-08 22:52:09 -0300

    release 1.0.0

---
+++ README.md
@@ -0,0 +1,13 @@
+#  Memorize app
+
+## This repository contains the result of Stanford material 01 exercise
+
+- [Class video](https://www.youtube.com/watch?v=bqu6BquVi2M)
+- [PDF With exercise](https://cs193p.sites.stanford.edu/sites/g/files/sbiybj16636/files/media/file/assignment_1.pdf)
+
+#### This exercise is pretty good to learn the basics about SwiftUI, if you want extract the best about the concepts of SwiftUI I recommend you watch and understand the video first and then try out the exercise from scratch
+
+
+## App overview
+![Overview](./memorize.gif)
+
--- memorize-study/ContentView.swift
+++ memorize-study/ContentView.swift
@@ -1,21 +1,112 @@
-//
-//  ContentView.swift
-//  memorize-study
-//
-//  Created by Vinicius  Santos on 06/02/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let vehiclesCards = ["✈️", "🚇", "🚉", "🚄", "⛴️", "🛶", "🛩️", "🛳️", "🚆","🛬", "🛺"]
+    
+    let peopleCards = ["🏋🏾", "🪂", "🧘🏼‍♂️","🧍🏽‍♂️", "👫","👩🏼‍🦼","👩🏼‍🚀", "🧑🏾‍🔧", "👨🏿‍🔧", "🎅🏼"]
+    
+    let fruitsCards = ["🍎", "🍏", "🍐", "🍊", "🍋", "🍌", "🍅", "🥔", "🥕", "🥥", "🍆", "🥑", "🍉", "🍇", "🍒", "🍈", "🥒","🌽"]
+    
+    @State var selectedCardsToShow: [String]
+    @State var cardsColors: Color
+    
+    let columns = [
+        GridItem(),
+        GridItem(),
+        GridItem(),
+        ]
+    
+    init() {
+        selectedCardsToShow = vehiclesCards
+        cardsColors = Color.red
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Memorize!")
+                .font(.largeTitle)
+                .fontWeight(.regular)
+            
+            Spacer()
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(selectedCardsToShow.shuffled(), id: \.self)
+                    { element in
+                        Card(icon: element, cardColor: cardsColors)
+                    }
+                }
+
+            }
+            
+            Spacer()
+            
+            HStack {
+                Tab(tabIcon: "car", tabLabel: "Vehicles", onTap: {
+                    selectedCardsToShow = vehiclesCards
+                    cardsColors = Color.red
+                })
+                
+                Tab(tabIcon: "person", tabLabel: "People", onTap: {
+                    selectedCardsToShow = peopleCards
+                    cardsColors = Color.blue
+                })
+                
+                Tab(tabIcon: "basket", tabLabel: "Fruits", onTap: {
+                    selectedCardsToShow = fruitsCards
+                    cardsColors = Color.green
+                })
+                
+            }
+            .foregroundColor(.blue)
+            
         }
-        .padding()
+        
+    }
+}
+
+struct Card: View {
+    let icon: String
+    let cardColor: Color
+    @State var isShowingIcon = false
+    
+    
+    var body: some View {
+       CardStyle
+    }
+    
+    var CardStyle: some View {
+        ZStack {
+            if (isShowingIcon) {
+                RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(cardColor, lineWidth: 3).foregroundColor(.white)
+                Text(icon)
+                    .font(.largeTitle)
+                
+            } else {
+                RoundedRectangle(cornerRadius: 20, style: .continuous).foregroundColor(cardColor)
+            }
+        }
+        .frame(height: 130)
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .onTapGesture {
+            isShowingIcon = !isShowingIcon
+        }
+    }
+}
+
+struct Tab: View {
+    var tabIcon: String
+    var tabLabel: String
+    var onTap: () -> Void
+    
+    var body: some View {
+        VStack {
+            Button(action: onTap) {
+                Image(systemName: tabIcon)
+                    .font(.system(size: 30))
+            }
+            Text(tabLabel)
+        }
+        .padding(.horizontal)
     }
 }
 
