230cd919270cd2453ce8239a58299100dbe114ef
Vinicius Santos <viniciusantos.florencio@gmail.com>
2023-02-18 14:56:41 -0300

    refactor contentView file

--- memorize-study/ContentView.swift
+++ memorize-study/ContentView.swift
@@ -1,25 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    let vehiclesCards = ["✈️", "🚇", "🚉", "🚄", "⛴️", "🛶", "🛩️", "🛳️", "🚆","🛬", "🛺"]
-    
-    let peopleCards = ["🏋🏾", "🪂", "🧘🏼‍♂️","🧍🏽‍♂️", "👫","👩🏼‍🦼","👩🏼‍🚀", "🧑🏾‍🔧", "👨🏿‍🔧", "🎅🏼"]
-    
-    let fruitsCards = ["🍎", "🍏", "🍐", "🍊", "🍋", "🍌", "🍅", "🥔", "🥕", "🥥", "🍆", "🥑", "🍉", "🍇", "🍒", "🍈", "🥒","🌽"]
-    
-    @State var selectedCardsToShow: [String]
-    @State var cardsColors: Color
+    @ObservedObject var memorizeGame = MemorizeModelView()
     
     let columns = [
         GridItem(),
         GridItem(),
         GridItem(),
         ]
-    
-    init() {
-        selectedCardsToShow = vehiclesCards
-        cardsColors = Color.red
-    }
     
     var body: some View {
         VStack {
@@ -28,46 +16,48 @@ struct ContentView: View {
                 .fontWeight(.regular)
             
             Spacer()
+            
             ScrollView {
                 LazyVGrid(columns: columns) {
-                    ForEach(selectedCardsToShow.shuffled(), id: \.self)
-                    { element in
-                        Card(icon: element, cardColor: cardsColors)
-                    }
+                    ForEach(memorizeGame.deckOfCards, id: \.id, content: {
+                        element in CardView(content: element.content, isShowingContent: element.isShowing, onTap: {
+                            memorizeGame.onTapCard(element.id)
+                        })
+                    })
                 }
 
             }
             
             Spacer()
             
-            HStack {
-                Tab(tabIcon: "car", tabLabel: "Vehicles", onTap: {
-                    selectedCardsToShow = vehiclesCards
-                    cardsColors = Color.red
-                })
-                
-                Tab(tabIcon: "person", tabLabel: "People", onTap: {
-                    selectedCardsToShow = peopleCards
-                    cardsColors = Color.blue
-                })
-                
-                Tab(tabIcon: "basket", tabLabel: "Fruits", onTap: {
-                    selectedCardsToShow = fruitsCards
-                    cardsColors = Color.green
-                })
-                
-            }
-            .foregroundColor(.blue)
-            
+//            HStack {
+//                TabView(tabIcon: "car", tabLabel: "Vehicles", onTap: {
+//                    selectedCardsToShow = vehiclesCards
+//                    cardsColors = Color.red
+//                })
+//
+//                TabView(tabIcon: "person", tabLabel: "People", onTap: {
+//                    selectedCardsToShow = peopleCards
+//                    cardsColors = Color.blue
+//                })
+//
+//                TabView(tabIcon: "basket", tabLabel: "Fruits", onTap: {
+//                    selectedCardsToShow = fruitsCards
+//                    cardsColors = Color.green
+//                })
+//
+//            }
+//            .foregroundColor(.blue)
+//
         }
         
     }
 }
 
-struct Card: View {
-    let icon: String
-    let cardColor: Color
-    @State var isShowingIcon = false
+struct CardView: View {
+    let content: String
+    let isShowingContent: Bool
+    let onTap: () -> Void
     
     
     var body: some View {
@@ -76,24 +66,22 @@ struct Card: View {
     
     var CardStyle: some View {
         ZStack {
-            if (isShowingIcon) {
-                RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(cardColor, lineWidth: 3).foregroundColor(.white)
-                Text(icon)
+            if (isShowingContent) {
+                RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(.red, lineWidth: 3).foregroundColor(.white)
+                Text(content)
                     .font(.largeTitle)
                 
             } else {
-                RoundedRectangle(cornerRadius: 20, style: .continuous).foregroundColor(cardColor)
+                RoundedRectangle(cornerRadius: 20, style: .continuous).foregroundColor(.red)
             }
         }
         .frame(height: 130)
         .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-        .onTapGesture {
-            isShowingIcon = !isShowingIcon
-        }
+        .onTapGesture { onTap() }
     }
 }
 
-struct Tab: View {
+struct TabView: View {
     var tabIcon: String
     var tabLabel: String
     var onTap: () -> Void
